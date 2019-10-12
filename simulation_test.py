b9f5634dcb0a578b7183af54b4bde325aed31555
liyaSileshi <liyasileshi@gmail.com>
2019-10-11 19:06:38 -0700

    simulation test

--- simulation_test.py
+++ simulation_test.py
@@ -2,6 +2,7 @@ import virus
 import person
 import simulation
 import pytest
+import os
 
 virus = simulation.Virus('Ebola', 0.25, 0.7)
 sim = simulation.Simulation(100,0.5,virus,10)
@@ -10,7 +11,14 @@ def test_simulation_instance():
     # Test instantiation without error
     sim = simulation.Simulation(100,0.5,virus,10)
     assert sim
-
+    assert len(sim.population) == 100
+    assert sim.pop_size == 100
+    assert sim.virus.name == "Ebola"
+    assert sim.virus.mortality_rate == 0.7
+    assert sim.virus.repro_rate == 0.25
+    assert sim.total_dead == 0
+    assert sim.total_infected == 10
+    assert sim.vaccine_saves == 0
 
 def test_create_population():
     population = sim._create_population(5)
@@ -19,4 +27,25 @@ def test_create_population():
         if i < 5:
             person = population[i]
             assert person._id == i
+        normal = 0
+        vacc = 0
+        infected = 0
+        for person in sim.population:
+            if person.is_vaccinated:
+                vacc += 1
 
+            elif person.infection:
+                infected += 1
+            else:
+                normal += 1
+
+    assert normal == 40
+    assert infected == 10
+    assert vacc == 50
+
+def test_simulation_should_continue():
+    assert sim._simulation_should_continue() == True
+    for sims in sim.population:
+        sims.is_alive = False
+    assert sim._simulation_should_continue() == False
+    
